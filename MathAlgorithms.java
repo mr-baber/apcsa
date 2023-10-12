e0af83527963c3a5bfc83a27e2135d49ca059d05
mr-baber <133150947+mr-baber@users.noreply.github.com>
2023-10-12 18:35:55 +0000

    Added MathAlgorithms starter code in class file

---
+++ MathAlgorithms.java
@@ -0,0 +1,13 @@
+public class MathAlgorithms{
+
+    public static void printDigits(int num){
+        System.out.println(num);
+    }
+
+    public static boolean isPrime(int num){
+        return false;
+    }
+    public static boolean isPerfect(int num){
+        return false;
+    }
+}
